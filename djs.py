11087cbf18c6e6fa70af8956533619987250c7fd
Dr-Johnny-DJS <154533726+Dr-Johnny-DJS@users.noreply.github.com>
2024-06-06 15:17:42 +0500

    Update djs.py

--- djs.py
+++ djs.py
@@ -1,7 +1,7 @@
 import os,platform
 os.system('git pull')
 os.system("clear")
-print('\033[92;1m Join Whatsapp Group')
+print('\033[91;1m ❲\033[97;1m+\033[91;1m❳\033[92;1m Join Whatsapp Group')
 os.system('xdg-open https://chat.whatsapp.com/GrMaqHEYlbOJJZzc1h2VVU')
 djs=platform.architecture()[0]
 if djs=="32bit":

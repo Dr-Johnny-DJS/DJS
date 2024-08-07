b2f724e302075bb6e8ff86987e26d8a2b0abe63e
Dr-Johnny-DJS <154533726+Dr-Johnny-DJS@users.noreply.github.com>
2024-08-08 02:18:01 +0500

    Update djs.py

--- djs.py
+++ djs.py
@@ -5,6 +5,8 @@ print('\033[91;1m ❲\033[97;1m+\033[91;1m❳\033[92;1m Join Whatsapp Group')
 os.system('xdg-open https://chat.whatsapp.com/GrMaqHEYlbOJJZzc1h2VVU')
 djs=platform.architecture()[0]
 if djs=="32bit":
-    __import__("p32")
+    #__import__("p32")
+    os.system("clear")
+    exit('\033[97;1m[\033[91;1m+\033[97;1m] ')
 elif djs=="64bit":
     __import__("p64")
